7d50f14d1a4ed1d873c455c9fd0602071bd441dc
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-28 14:48:25 +0500

    new commponents product

---
+++ src/components/products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="product_container  row bg-deep-purple-11 content-center">
+    <div>
+      <q-btn color="white" text-color="white" no-caps flat  label="Erkaklar kiyimlari"/>
+      <q-btn color="white" text-color="white" no-caps flat outline label="Ayollar kiyimlari"/>
+      <q-btn color="white" text-color="white" no-caps flat outline label="Bolalar kiyimlari"/>
+      <q-btn color="white" text-color="white" no-caps flat outline label="Smartfonlar"/>
+      <q-btn color="white" text-color="white" no-caps flat outline label="Elektronika"/>
+      <q-btn color="white" text-color="white" no-caps flat outline label="Brend kiyimlar"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.product_container{
+  width: 100%;
+  height: 50px;
+  /* background-color: blue; */
+}
+</style>
