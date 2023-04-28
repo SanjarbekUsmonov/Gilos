d75a757f56238da62abf5eb60dd79da6933494e9
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-28 14:51:33 +0500

    splice div

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,34 +1,22 @@
 <template>
-  <div>
-    <section>
       <div class="container">
+        <products/>
         <carusel/>
-        <div>
           <boys />
-        </div>
         <div class="clothes q-pa-md">
           <q-img style="cursor: pointer;"
             src="https://images.uzum.uz/cetufm0l08k0o9qhg7d0/main_page_banner.jpg"
           />
         </div>
-        <div>
           <smartphones />
-        </div>
         <div class="clothes q-pa-md" >
           <q-img style="cursor: pointer;"
             src="https://images.uzum.uz/cetug62vtie1lhbghhn0/main_page_banner.jpg"
           />
         </div>
-        <div>
-
-          <womens />
-        </div>
-        <div>
+          <womens/>
           <childern/>
-        </div>
       </div>
-    </section>
-  </div>
 </template>
 
 <script setup>
@@ -37,13 +25,15 @@ import carusel from "../components/carusel";
 import Smartphones from "../components/smartphones";
 import Womens from "../components/womens";
 import Childern from "../components/childern";
+import products from "../components/products"
 </script>
 
 <style lang="css" scoped>
 .container {
-  width: 90%;
+  width: 94%;
   height: auto;
   margin: 0 auto;
-  background: rgb(255, 255, 255);
+  background: white;
+
 }
 </style>
