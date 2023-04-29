d9131e2d53127fb387315cecf2279de23a09dd3e
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-29 16:14:38 +0500

    format cod

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,22 +1,22 @@
 <template>
-      <div class="container">
-        <products/>
-        <carusel/>
-          <boys />
-        <div class="clothes q-pa-md">
-          <q-img style="cursor: pointer;"
-            src="https://images.uzum.uz/cetufm0l08k0o9qhg7d0/main_page_banner.jpg"
-          />
-        </div>
-          <smartphones />
-        <div class="clothes q-pa-md" >
-          <q-img style="cursor: pointer;"
-            src="https://images.uzum.uz/cetug62vtie1lhbghhn0/main_page_banner.jpg"
-          />
-        </div>
-          <womens/>
-          <childern/>
-      </div>
+  <div class="container">
+    <products />
+    <carusel />
+    <boys />
+    <div class="clothes q-pa-md">
+      <router-link to="/BrendKiyimlar">
+        <q-img style="cursor: pointer" src="https://images.uzum.uz/cetufm0l08k0o9qhg7d0/main_page_banner.jpg" />
+      </router-link>
+    </div>
+    <smartphones />
+    <div class="clothes q-pa-md">
+      <router-link to="/Elektronika">
+        <q-img style="cursor: pointer" src="https://images.uzum.uz/cetug62vtie1lhbghhn0/main_page_banner.jpg" />
+      </router-link>
+    </div>
+    <womens />
+    <childern />
+  </div>
 </template>
 
 <script setup>
@@ -25,7 +25,10 @@ import carusel from "../components/carusel";
 import Smartphones from "../components/smartphones";
 import Womens from "../components/womens";
 import Childern from "../components/childern";
-import products from "../components/products"
+import products from "../components/products";
+import {useCounterStore} from '../stores/index'
+const store = useCounterStore()
+store.GetAPI()
 </script>
 
 <style lang="css" scoped>
@@ -34,6 +37,5 @@ import products from "../components/products"
   height: auto;
   margin: 0 auto;
   background: white;
-
 }
 </style>
