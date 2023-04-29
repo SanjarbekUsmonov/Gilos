82a0d59833f3b0959ea59d977eba7d3fcafd8b60
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-29 16:13:25 +0500

    props and import api

--- src/pages/Elektronika.vue
+++ src/pages/Elektronika.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-
+    <div class="products">
+      <products/>
+    </div>
+   <div class="container">
+    <h3>Elektronika...</h3>
+      <div class="q-px-lg">
+        <card/>
+      </div>
+   </div>
   </div>
 </template>
 
-<script>
-  export default {
-
-  }
+<script setup>
+  import card from '../components/card.vue';
+  import products from '../components/products.vue'
 </script>
 
 <style lang="scss" scoped>
-
+.container{
+  padding: 20px;
+}
+h3{
+  padding-left: 13px;
+}
+.products{
+  margin: 0px 40px;
+}
 </style>
