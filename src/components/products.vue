4f0e86498719f88358756b422e3a04bc680e56b0
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-29 16:08:42 +0500

    format code

--- src/components/products.vue
+++ src/components/products.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="product_container  row bg-deep-purple-11 content-center">
     <div>
-      <q-btn color="white" text-color="white" no-caps flat  label="Erkaklar kiyimlari"/>
-      <q-btn color="white" text-color="white" no-caps flat outline label="Ayollar kiyimlari"/>
-      <q-btn color="white" text-color="white" no-caps flat outline label="Bolalar kiyimlari"/>
-      <q-btn color="white" text-color="white" no-caps flat outline label="Smartfonlar"/>
-      <q-btn color="white" text-color="white" no-caps flat outline label="Elektronika"/>
-      <q-btn color="white" text-color="white" no-caps flat outline label="Brend kiyimlar"/>
+      <router-link to="/ErkaklarKiyimi"> <q-btn color="white" text-color="white" no-caps flat
+          label="Erkaklar kiyimlari" /></router-link>
+      <router-link to="/AyollarKiyimi"><q-btn color="white" text-color="white" no-caps flat outline
+          label="Ayollar kiyimlari" /></router-link>
+      <router-link to="/BolalarKiyimi"><q-btn color="white" text-color="white" no-caps flat outline
+          label="Bolalar kiyimlari" /></router-link>
+      <router-link to="/Smartfonlar"><q-btn color="white" text-color="white" no-caps flat outline
+          label="Smartfonlar" /></router-link>
+      <router-link to="/Elektronika"><q-btn color="white" text-color="white" no-caps flat outline
+          label="Elektronika" /></router-link>
+      <router-link to="/BrendKiyimlar"> <q-btn color="white" text-color="white" no-caps flat outline
+          label="Brend kiyimlar" /></router-link>
     </div>
   </div>
 </template>
@@ -16,9 +22,8 @@
 </script>
 
 <style scoped>
-.product_container{
+.product_container {
   width: 100%;
   height: 50px;
   /* background-color: blue; */
-}
-</style>
+}</style>
