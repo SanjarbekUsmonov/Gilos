7214ef5b8541acf53cb0c058ca0ab38137e34b5b
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-29 16:09:14 +0500

    props and import api

--- src/components/smartphones.vue
+++ src/components/smartphones.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <div>
-      <h3 style="font-weight: 450;">Smartfonlar...</h3>
+      <router-link to="/Smartfonlar">
+        <h3>Smartfonlar...</h3>
+      </router-link>
     </div>
-    <card />
+    <div>
+      <card :products = "store.smartphones"/>
+    </div>
   </div>
 </template>
 
 <script setup>
 import card from './card.vue';
-import {useCounterStore} from "src/stores/index"
+import { useCounterStore } from "src/stores/index"
 const store = useCounterStore()
 
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
