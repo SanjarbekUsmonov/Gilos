bca8898cf5c90aff5cb840112d47059059a7bce9
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-29 16:17:20 +0500

    api alin = array

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,17 +2,37 @@ import { defineStore, createPinia } from 'pinia';
 import {store} from 'quasar/wrappers'
 import axios from 'axios'
 
-
+const API_URL = "http://bazarcom.pythonanywhere.com";
 export const useCounterStore = defineStore('store', {
   state: () => ({
-
-
+  cotegory:  [],
+  boys: [],
+  womans: [],
+  children: [],
+  smartphones: [],
+  searchPanel: []
   }),
+
   getters: {
-
   },
-  actions: {
 
+  actions: {
+   async GetAPI(){
+      let appInfo = await axios.get("http://bazarcom.pythonanywhere.com/category/");
+      this.cotegory = appInfo.data;
+      //console.log(this.cotegory);
+      this.boys = this.cotegory[0].products;
+      this.womans = this.cotegory[1].products;
+      this.children = this.cotegory[2].products;
+      this.smartphones = this.cotegory[3].products
+      console.log(this.boys)
+
+   },
+   async GETPRODUCTS(){
+      let appProducts = await axios.get("http://bazarcom.pythonanywhere.com/products/")
+      this.searchPanel = [...appProducts.data]
+      console.log(this.searchPanel)
+   }
   },
 
 });
