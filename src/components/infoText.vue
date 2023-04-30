1f3e520361252aa207f10aa0a766a8b53ded4d5d
SanjarbekUsmonov <noobpusk@gmail.com>
2023-04-30 22:33:16 +0500

    new function inc and dec

--- src/components/infoText.vue
+++ src/components/infoText.vue
@@ -1,39 +1,53 @@
 <template>
   <div>
-    <div class="info-text text-h5 text-weight-medium">
-      sasasasasasas
-    </div>
+    <div class="info-text text-h5 text-weight-medium">sasasasasasas</div>
     <div class="info-data q-mt-md">
-      <div><span>Sotuvchi:</span> <span class="q-ml-xl">Nomalum</span></div>
-      <div><span>Yetkazib berish:</span> <span>1 kun, bepul</span></div>
+      <div class="text-grey-8">
+        <span>Sotuvchi:</span> <span class="q-ml-xl">Nomalum</span>
+      </div>
+      <div class="text-grey-8">
+        <span>Yetkazib berish:</span> <span>1 kun, bepul</span>
+      </div>
     </div>
     <q-separator class="q-mt-md" />
     <div class="info-amount">
-      <div class="q-mt-lg">Miqdor:</div>
-      <div class="info-amount-active"></div>
-      <div class="info-amount-cost q-mt-lg">
-        Narx: 50000 so'm
-        <div class="info-amount-cost-active text-h6">
-          5000
-        </div>
+      <div class="q-mt-lg text-h6"><b>Miqdor:</b></div>
+      <div
+        class="info-amount-active bg-indigo-1 row justify-between items-center content-center"
+      >
+        <q-btn @click="piniaStore.dec()" color="red" glossy label="-" />
+        <strong>{{ piniaStore.counter }}</strong>
+        <q-btn @click="piniaStore.inc()" color="green" glossy label="+" />
+      </div>
+      <div class="info-amount-cost q-mt-lg text-h6">Narx: 50000 so'm</div>
+      <div class="info-amount-cost-active text-grey-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, quos
+        ipsa dolor quam enim quod!
       </div>
     </div>
     <div class="info-btn q-mt-xl">
-      <q-btn color="primary" glossy label="Savatga qo'shish" padding="10px 70px" />
-      <q-btn color="white" glossy text-color="black" label="Darhol sotib olish" padding="10px 70px" class="q-ml-md" />
+      <q-btn
+        color="indigo-4"
+        glossy
+        label="Savatga qo'shish"
+        padding="10px 70px"
+      />
+      <q-btn
+        color="white"
+        glossy
+        text-color="black"
+        label="Darhol sotib olish"
+        padding="10px 70px"
+        class="q-ml-md"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-
-
+import { useCounterStore } from "../stores/index";
+const piniaStore = useCounterStore();
 </script>
 
 <style lang="scss" scoped>
-.info-amount-active {
-  width: 120px;
-  height: 40px;
-  background: black;
-}
 </style>
