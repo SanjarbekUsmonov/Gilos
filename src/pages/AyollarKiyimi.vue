49ffe8bac1ab50ec8024291ac21de74faffd2703
SanjarbekUsmonov <noobpusk@gmail.com>
2023-05-03 02:08:44 +0500

    store function camleCase usuliga o'zgardi

--- src/pages/AyollarKiyimi.vue
+++ src/pages/AyollarKiyimi.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
     <div class="products">
-      <products/>
+      <products />
     </div>
-   <div class="container">
-    <h3>Ayollar kiyimlari...</h3>
-     <div class="q-px-lg">
-      <card :products="store.womans"/>
-     </div>
-   </div>
+    <div class="container">
+      <h3>Ayollar kiyimlari...</h3>
+      <div class="q-px-lg">
+        <card :products="store.womans" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import card from '../components/card.vue';
-  import products from '../components/products.vue'
-  import {useCounterStore} from '../stores/index'
-  const store = useCounterStore()
-  store.GetAPI()
+import card from "../components/card.vue";
+import products from "../components/products.vue";
+import { useCounterStore } from "../stores/index";
+const store = useCounterStore();
+store.getApi();
 </script>
 
 <style lang="scss" scoped>
-.container{
+.container {
   padding: 20px;
 }
-h3{
+h3 {
   padding-left: 13px;
 }
-.products{
+.products {
   margin: 0px 40px;
 }
 </style>
