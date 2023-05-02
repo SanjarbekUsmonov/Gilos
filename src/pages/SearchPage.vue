8870012a3c25eb41d4612e875cc5a9cf39be0b2c
SanjarbekUsmonov <noobpusk@gmail.com>
2023-05-03 02:09:38 +0500

    store function camleCase usuliga o'zgardi

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="q-px-lg">
-    <Card :products = "store.searchPanel"/>
+    <Card :products="store.searchPanel" />
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-import Card from 'src/components/card.vue';
-import {useCounterStore} from '../stores/index'
-  const store = useCounterStore()
-   store.GETPRODUCTS()
+import { ref } from "vue";
+import Card from "src/components/card.vue";
+import { useCounterStore } from "../stores/index";
+const store = useCounterStore();
+store.getProducts();
 </script>
 
 <style lang="css" scoped>
-div{
+div {
   padding: 20px;
 }
 </style>
