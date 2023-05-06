f3470048402461b6dae680ef75dfd002a143d98b
SanjarbekUsmonov <noobpusk@gmail.com>
2023-05-06 14:07:53 +0500

    qiymatlar ko'paytirildi

--- src/components/infoText.vue
+++ src/components/infoText.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <div class="info-text text-h5 text-weight-medium">sasasasasasas</div>
+  <div v-if="products != null">
+    <div class="info-text text-h5 text-weight-medium">
+      {{ products.name }}
+    </div>
     <div class="info-data q-mt-md">
       <div class="text-grey-8">
         <span>Sotuvchi:</span> <span class="q-ml-xl">Nomalum</span>
@@ -19,25 +21,29 @@
         <strong>{{ piniaStore.counter }}</strong>
         <q-btn @click="piniaStore.inc()" color="green" glossy label="+" />
       </div>
-      <div class="info-amount-cost q-mt-lg text-h6">Narx: 50000 so'm</div>
+      <div class="info-amount-cost q-mt-lg text-h6">
+        Narxi:
+        <span class="text-grey-6">
+          {{ products.prince * piniaStore.counter }} so'm</span
+        >
+      </div>
       <div class="info-amount-cost-active text-grey-8">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, quos
-        ipsa dolor quam enim quod!
+        {{ products.info }}
       </div>
     </div>
-    <div class="info-btn q-mt-xl">
+    <div class="info-btn q-mt-xl row">
       <q-btn
         color="indigo-4"
         glossy
         label="Savatga qo'shish"
-        padding="10px 70px"
+        padding="10px 20px"
       />
       <q-btn
         color="white"
         glossy
         text-color="black"
         label="Darhol sotib olish"
-        padding="10px 70px"
+        padding="10px 20px"
         class="q-ml-md"
       />
     </div>
@@ -47,6 +53,7 @@
 <script setup>
 import { useCounterStore } from "../stores/index";
 const piniaStore = useCounterStore();
+const props = defineProps({ products: Object });
 </script>
 
 <style lang="scss" scoped>
