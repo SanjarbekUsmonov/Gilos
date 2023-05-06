8d2cf158f34953d204745de6f52ed93e3e060278
SanjarbekUsmonov <noobpusk@gmail.com>
2023-05-06 14:08:34 +0500

    router link qo'yildi

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,53 +1,50 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
-
-      <q-header reveal
-        bordered
-        class=" header bg-deep-purple-11 ">
+    <q-header reveal bordered class="header bg-deep-purple-11">
       <q-toolbar>
         <q-toolbar-title class="row justify-between items-center">
-          <router-link to="/" class="black text-h5 text-weight-bold">Gilos.Market</router-link>
-        <router-link to="/search">
-          <q-input v-model="search" filled type="search" class="input_header"  bg-color="indigo-1"  color="white">
-          <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-         </q-input>
-        </router-link>
-          <q-btn flat round icon="event" color="white"/>
+          <router-link to="/" class="black text-h5 text-weight-bold"
+            >Gilos.Market</router-link
+          >
+          <router-link to="/search">
+            <q-input
+              v-model="search"
+              filled
+              type="search"
+              class="input_header"
+              bg-color="indigo-1"
+              color="white"
+            >
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </router-link>
+          <q-btn flat round icon="event" color="white" />
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-
-
-
     <q-page-container>
       <router-view />
     </q-page-container>
-
-
-
   </q-layout>
 </template>
 
 <script setup>
 import { ref } from "vue";
-let search = ref('')
+let search = ref("");
 </script>
 <style scoped>
-.header{
+.header {
   width: 94%;
   margin: 0 auto;
 }
-.black{
- color: white;
+.black {
+  color: white;
 }
-.input_header{
+.input_header {
   width: 400px;
   margin: 10px;
 }
-
-
 </style>
