8464368c3208c4324cfe2cf77b2f856da000beb9
SanjarbekUsmonov <noobpusk@gmail.com>
2023-05-06 16:14:19 +0500

    comentariya qoldirildi

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -10,6 +10,22 @@ import Card from "src/components/card.vue";
 import { useCounterStore } from "../stores/index";
 const store = useCounterStore();
 store.getProducts();
+
+// let person = {
+//   name: "Ali",
+//   surname: "Madiminov",
+// };
+// person.age = 22;
+// console.log(person);
+
+// var obj = { key1: "value1", key2: "value2" };
+// var pair = { key3: "value3" };
+// var scalar = "value4";
+// obj = { ...obj, ...pair, scalar };
+
+// console.log(obj);
+// var obj = {key1: "value1", key2: "value2"};
+// Object.assign(obj, {key3: "value3"});
 </script>
 
 <style lang="css" scoped>
